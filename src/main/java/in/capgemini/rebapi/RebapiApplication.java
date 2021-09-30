8d2486e9ba8121a881bd8a6c17d2d427e2d2b416
Jisha <Jisha@LAPTOP-GFJGC0F6.bbrouter>
2021-09-30 15:22:12 +0530

    first commit.

---
+++ src/main/java/in/capgemini/rebapi/RebapiApplication.java
@@ -0,0 +1,13 @@
+package in.capgemini.rebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/capgemini/rebapi/RebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.capgemini.rebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
